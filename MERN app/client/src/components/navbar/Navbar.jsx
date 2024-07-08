14ffa7307caa089aa03ecd811f010be848e81e8c
Sanjana0826 <sanjanadeshan0826@gmail.com>
2024-07-09 02:47:42 +0530

    change the icon

--- MERN app/client/src/components/navbar/Navbar.jsx
+++ MERN app/client/src/components/navbar/Navbar.jsx
@@ -22,7 +22,7 @@ function Navbar() {
       <div className="left">
         <a href="/" className="logo">
           <img src="/logo.png" alt="" />
-          <span>LamaEstate</span>
+          <span>Estate Agency</span>
         </a>
         <a href="/">Home</a>
         <a href="/">About</a>
